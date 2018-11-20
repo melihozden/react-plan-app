8d23093da2e070fe1ca635ab85962a8bf34286c1
melihozden <melihozden403@gmail.com>
2018-11-20 23:35:47 +0300

    login screen is done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import "../css/login.css"; 
+import logo from "../images/whitetrans2x.png"
+import logo2 from "../images/blacktransx.png"
+
 // import {BrowserRouter as Router, Route} from 'react-router-dom';
 class Login extends Component {
+
+
+
+
     render() {
+
+
         return (
             <div className="login-box">
     <div className="left-div">
         <div className="dark-logo">
-            
+          <img className="logo-des" src={logo} alt="logo" height="50" width="75" />
         </div>
     </div>
     <div className="right-div">
+      <img className="logo-des-2" src={logo2} alt="logo" height="50" width="75" />    
     <h3 className="title-name">Welcome PlanWork</h3>
     <form className="email-login">
       <div className="u-form-group">
@@ -31,7 +41,7 @@ class Login extends Component {
       <hr className="seperator"/>
       <p className="login-with">-Or connect with-</p>
       <div className="u-form-group">
-        <button className="google-button"><i className="fab fa-google-plus-g"> Google+</i></button>
+        <button className="google-button"><i className="fab fa-google-plus-g"></i>Google+</button>
         <button className="facebook-button"><i className="fab fa-facebook"></i> Facebook</button>
         <button className="twitter-button"><i className="fab fa-twitter"></i> Twitter</button>
     </div>
